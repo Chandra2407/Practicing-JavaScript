b4dfeb7ec35d111b84c3403b6a3857d521e4084a
GrayDragneel <49267139+Chandra2407@users.noreply.github.com>
2021-07-16 19:00:58 +0530

    Mostly Javascript Changes

--- src/index.js
+++ src/index.js
@@ -1,1 +1,47 @@
 console.log("Hello");
+let x = 10;
+var z = x + 5;
+console.log(z);
+{
+  let x = 20;
+  console.log(x);
+}
+console.log(x);
+// arrays and constants
+const cars = ["abc", "xyz"];
+cars[0] = "lmn";
+cars.push("jk");
+console.log(cars);
+
+// objects and constants
+
+const bikes = { type: "2wheels", owner: "x" };
+console.log(bikes);
+bikes.owner = "y";
+bikes.color = "black";
+console.log(bikes);
+console.log(typeof bikes);
+
+// functions
+
+function func() {
+  return x;
+}
+console.log(func());
+let text = "a book";
+
+// String
+
+console.log(text.replace("book", "boy"));
+console.log(text);
+console.log(text.slice(0, 2));
+console.log(text);
+let a = "a man is a dog";
+const b = a.split(" ");
+console.log(b);
+console.log(b[0]);
+console.log(a.indexOf("a"));
+console.log(a.search("dog"));
+console.log(a.match(/a/gi));
+
+console.log(typeof NaN);
